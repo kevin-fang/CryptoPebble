4b59d9dded852c438e41223b01499375937f4448
Kevin Fang <kfang1233@gmail.com>
2017-07-30 14:53:15 -0400

    Initial commit with implementation from Poloniex

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var Vibe = require('ui/vibe');
+
+var card = new UI.Card({
+  title: 'CryptoTicker',
+  subtitle: 'Loading..',
+  body: 'Please wait...'
+});
+
+card.show();
+
+var ajax = require('ajax');
+var poloniexUrl = "https://poloniex.com/public?command=returnTicker";
+/*jshint sub: true*/
+
+ajax({ url: poloniexUrl, type: 'json'},
+    function(data) {
+      var btcPrice = data['USDT_BTC'].last;
+      var ethPrice = data['USDT_ETH'].last;
+      
+      card.body("BTC: $" + btcPrice + '\nETH: $' + ethPrice);
+      card.title('CryptoTicker');
+      card.subtitle('');
+      Vibe.vibrate('short');
+      card.show();
+    });
