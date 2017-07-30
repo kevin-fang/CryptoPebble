2310d50ab45006943eab85537d4020804b9401eb
Kevin Fang <kfang1233@gmail.com>
2017-07-30 16:44:06 -0400

    Added select to refresh option

--- src/js/app.js
+++ src/js/app.js
@@ -7,26 +7,36 @@
 var UI = require('ui');
 var Vibe = require('ui/vibe');
 
-var card = new UI.Card({
+var main = new UI.Card({
   title: 'CryptoTicker',
   subtitle: 'Loading..',
   body: 'Please wait...'
 });
 
-card.show();
+main.show();
 
 var ajax = require('ajax');
 var poloniexUrl = "https://poloniex.com/public?command=returnTicker";
 /*jshint sub: true*/
 
-ajax({ url: poloniexUrl, type: 'json'},
-    function(data) {
-      var btcPrice = data['USDT_BTC'].last;
-      var ethPrice = data['USDT_ETH'].last;
-     // var ltcPrice = data['USDT_LTC'].last;
-      
-      card.body("BTC: $" + btcPrice + '\nETH: $' + ethPrice);
-      card.title('CryptoTicker');
-      card.subtitle('');
-      Vibe.vibrate('short');
-    });
+function updateScreen() {
+  ajax({ url: poloniexUrl, type: 'json'},
+      function(data) {
+        var btcPrice = data['USDT_BTC'].last;
+        var ethPrice = data['USDT_ETH'].last;
+       // var ltcPrice = data['USDT_LTC'].last;
+        
+        main.body("BTC: $" + btcPrice + '\nETH: $' + ethPrice);
+        main.title('CryptoTicker');
+        main.subtitle('');
+        Vibe.vibrate('short');
+      });
+}
+
+updateScreen();
+
+main.on('click', 'select', function(e) {
+  main.subtitle("Loading");
+  main.body("Please wait...");
+  updateScreen();
+});
